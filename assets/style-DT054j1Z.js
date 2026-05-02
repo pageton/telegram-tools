26d6e3ec37935c93fca0768561e214142146ce85
Sadiq <pageton@proton.me>
2026-05-03 00:25:35 +0300

    chore: deploy to GitHub Pages

---
+++ assets/style-DT054j1Z.js
@@ -0,0 +1,1 @@
+import{a4 as y}from"./index-92QoR6xi.js";function f(e,s,i,r){var l=e.__style;if(l!==s){var t=y(s);t==null?e.removeAttribute("style"):e.style.cssText=t,e.__style=s}return r}export{f as s};
